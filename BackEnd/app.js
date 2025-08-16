82ebe4daff11f195dde52f37033048045303f1ec
ChandanMahto <chandan.ckp11@gmail.com>
2025-08-16 23:41:47 +0530

    removed default routing

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', indexRouter);
+// app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/pizza',pizzaRouter);
 app.use('/addToCart',pizzaRouter);
